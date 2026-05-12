349d66174c67fb1338f9128a6b31737bba594516
tamil-12 <tamilselvan92485@gmail.com>
2026-05-12 13:40:03 +0530

    Fix MongoDB environment variable

--- db.js
+++ db.js
@@ -1,26 +1,9 @@
-// const mongoose = require("mongoose");
-// require("dotenv").config()
-// mongoose.connect(process.env.connectionString)
-// .then(()=>{
-//     console.log("mongodb is connected");  
-// })
-// .catch((err)=>{
-//     console.log("mongodb connection error:", err.message);  
-// })
-import mongoose from "mongoose";
-
-const connectDB = async () => {
-  try {
-    console.log("ENV VALUE:", process.env.connectionString);
-
-    await mongoose.connect(process.env.connectionString, {
-      serverSelectionTimeoutMS: 5000
-    });
-
-    console.log("MongoDB Connected Successfully");
-  } catch (error) {
-    console.error("MongoDB connection error:", error.message);
-  }
-};
-
-export default connectDB;
+const mongoose = require("mongoose");
+require("dotenv").config()
+mongoose.connect(process.env.connectionString)
+.then(()=>{
+    console.log("mongodb is connected");  
+})
+.catch((err)=>{
+    console.log("mongodb connection error:", err.message);  
+})
